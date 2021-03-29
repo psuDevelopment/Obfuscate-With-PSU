180e1fe23347e6434888d8e89be78ed81259483e
ColdDev <colddev2@pm.me>
2021-03-29 22:45:28 +0100

    feat: Add proper error handling if file not found

--- README.md
+++ README.md
@@ -3,45 +3,51 @@
 This is a GitHub action that allows you to integrate PSU obfuscation into your pipeline.
 
 ### Prerequisites
-- A PSU Premium subscription
-- Your API key
-- Your Lua script to be obfuscated
+
+-   A PSU Premium subscription
+-   Your API key
+-   Your Lua script to be obfuscated
 
 ### Known Caveats
+
 Multiple files cannot be obfuscated at once at this time - this issue will be addressed in a later release of the action.
 
 # Usage
 
 ### Prerequisites
+
 Create a workflow `.yml` file in `.github/workflows` inside of your repository. An example workflow can be found below.
 
 ### Inputs
-- `file` - The path to the file to be obfuscated
-- `apiKey` - Your PSU API key, this can be specified through GitHub's secrets.
+
+-   `file` - The path to the file to be obfuscated
+-   `apiKey` - Your PSU API key, this can be specified through GitHub's secrets.
 
 ### Outputs
-- `file` - The obfuscated file
+
+-   `file` - The obfuscated file
 
 ### Example workflow
+
 ```yml
 name: Example Workflow
 on: push
 
 jobs:
-  obfuscate:
-    runs-on: ubuntu-latest
+    obfuscate:
+        runs-on: ubuntu-latest
 
-    steps:
-    - name: Checkout
-      uses: actions/checkout@master
-    - name: obfuscate
-      uses: psuDevelopment/Obfuscate-With-PSU@main
-      id: file
-      with:
-        path: ./Script.lua
-        apiKey: ${{ secrets.PSU_API_KEY }}
-    - name: Echo obfuscated file
-      run: echo ${{ steps.obfuscate.outputs.file }}
+        steps:
+            - name: Checkout
+              uses: actions/checkout@master
+            - name: obfuscate
+              uses: psuDevelopment/Obfuscate-With-PSU@main
+              id: file
+              with:
+                  path: ./Script.lua
+                  apiKey: ${{ secrets.PSU_API_KEY }}
+            - name: Echo obfuscated file
+              run: echo ${{ steps.obfuscate.outputs.file }}
 ```
 
 # Licensing
--- src/index.js
+++ src/index.js
@@ -2,44 +2,45 @@ const core = require("@actions/core");
 const path = require("path");
 const axios = require("axios");
 const os = require("os");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const getAbsolutePath = (filePath) => {
-  if (filePath[0] !== "~") return path.resolve(filePath);
+    if (filePath[0] !== "~") return path.resolve(filePath);
 
-  const homeDir = os.homedir();
-  if (homeDir) return path.join(homeDir, filePath.slice(1));
+    const homeDir = os.homedir();
+    if (homeDir) return path.join(homeDir, filePath.slice(1));
 
-  throw new Error("Unable to resolve '~' to HOME");
+    throw new Error("Unable to resolve '~' to HOME");
 };
 
 const run = async () => {
-  const filePath = getAbsolutePath(core.getInput("path", { required: true }));
-  core.debug(`Path: ${filePath}`);
+    const filePath = getAbsolutePath(core.getInput("path", { required: true }));
+    const script = await fs
+        .readFile(filePath, { encoding: "utf8" })
+        .catch((err) => {
+            throw new Error(err);
+        });
 
-  const script = fs.readFileSync(filePath, { encoding: "utf8" });
-  core.debug(`Text: ${script}`);
-
-  axios
-    .post("https://api.psu.dev/obfuscate", {
-      script,
-      key: core.getInput("apiKey"),
-      options: core.getInput("options"),
-    })
-    .then(({ data }) => {
-      if (data.status === "passed") core.setOutput("file", data.data);
-    })
-    .catch(({ response: data }) => {
-      if (data.status === "failed") {
-        if (!data.reason)
-          throw new Error(
-            "An unknown error occurred while obfuscating the script. Sorry for the inconvenience."
-          );
-        throw new Error(
-          `An error occurred while obfuscating your script: ${data.reason}`
-        );
-      }
-    });
+    axios
+        .post("https://api.psu.dev/obfuscate", {
+            script,
+            key: core.getInput("apiKey"),
+            options: core.getInput("options"),
+        })
+        .then(({ data }) => {
+            if (data.status === "passed") core.setOutput("file", data.data);
+        })
+        .catch(({ response: data }) => {
+            if (data.status === "failed") {
+                if (!data.reason)
+                    throw new Error(
+                        "An unknown error occurred while obfuscating the script. Sorry for the inconvenience."
+                    );
+                throw new Error(
+                    `An error occurred while obfuscating your script: ${data.reason}`
+                );
+            }
+        });
 };
 
 run();
