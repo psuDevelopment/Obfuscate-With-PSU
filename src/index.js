4c9c5dba50ff3acaf9aafa1a10124d9261dbdb62
ColdDev <colddev2@pm.me>
2021-03-29 16:04:32 +0100

    fix: Re-add getAbsolutePath

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ const axios = require("axios");
 const os = require("os");
 const fs = require("fs");
 
-// const getAbsolutePath = (filePath) => {
-//   if (filePath[0] !== "~") return path.resolve(filePath);
+const getAbsolutePath = (filePath) => {
+  if (filePath[0] !== "~") return path.resolve(filePath);
 
-//   const homeDir = os.homedir();
-//   if (homeDir) return path.join(homeDir, filePath.slice(1));
+  const homeDir = os.homedir();
+  if (homeDir) return path.join(homeDir, filePath.slice(1));
 
-//   throw new Error("Unable to resolve '~' to HOME");
-// };
+  throw new Error("Unable to resolve '~' to HOME");
+};
 
 const run = async () => {
-//   const filePath = getAbsolutePath(core.getInput("file", { required: true }));
-//   core.debug(`Path: ${filePath}`);
+  const filePath = getAbsolutePath(core.getInput("file", { required: true }));
+  core.debug(`Path: ${filePath}`);
 
-  const script = fs.readFileSync(core.getInput("file"), { encoding: "utf8" });
+  const script = fs.readFileSync(filePath, { encoding: "utf8" });
   core.debug(`Text: ${text}`);
 
   axios
